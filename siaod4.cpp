4ad4ba2ee14a0fb50b3db6c51997241ccb648125
Nelo5 <furmit@mail.ru>
2024-04-08 23:05:07 +0300

    322

--- siaod4.cpp
+++ siaod4.cpp
@@ -3,38 +3,82 @@
 #include<fstream>
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
-int get2word(string s) {
-	if (s == "") { return 10000000000000; }
-	int l = s.find(" ");
-	int r = s.find(" ", l + 1);
-	return stoi(s.substr(l + 1, r - l ));
+string get2word(string s) {
+	return s;
+	//if (s == "") { return "";}
+	//int l = s.find(" ");
+	//int r = s.find(" ", l + 1);
+	//return s.substr(l + 1, r - l);
 }
 
-void partition(string file1, string file2, string file3, int size) {
+bool compare_strings(string str1, string str2) {
+	for (int i = 0; i < min(str1.length(), str2.length()); i++) {
+		if (tolower(str2[i]) < tolower(str1[i])) {
+			return false;
+		}
+		else if (tolower(str2[i]) > tolower(str1[i])) {
+			return true;
+		}
+	}
+	return true;
+}
+void print_array(string* a,int n) {
+	for (int i = 0; i < n;i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+void quick_sort(string* a, int first, int last) {
+	int i = first;
+	int j = last;
+	string pivot = get2word(a[(i + j) / 2]);
+	cout << pivot << endl;
+	while (i <= j) {
+		//print_array(a, j - i+1);
+		while (get2word(a[i]).compare(pivot)<0) { i++; }
+		while (get2word(a[j]).compare(pivot) > 0) { j--; }
+		if (i <= j) {
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}
+	//print_array(a, last-first);
+	if (last > i) {
+		quick_sort(a, i, last);
+	}
+	if (first < j) {
+		quick_sort(a, first, j+1);
+	}
+}
+
+
+void partition(string file1, string file2, string file3, int size, int &lenB, int &lenC) {
 	ifstream A(file1);
 	ofstream B(file2);
 	ofstream C(file3);
 	int counter = 0;
 	string s = "";
 	while (getline(A, s)) {
-		if (counter/size % 2 == 0) {
+		if ((counter/size) % 2 == 0) {
+			cout << s<<endl;
 			B<<s<<endl;
-			cout << "b";
+			lenB++;
 		}
 		else {
-			C<<s<<endl;
-			cout << "c";
+			C << s << endl;
+			cout << s << endl;
+			lenC++;
 		}
 		counter++;
 	}
-	A.close();
-	B.close();
-	C.close();
+	cout << counter<<endl;
 }
 
-void merge(string file1, string file2, string file3, int size) {
+void merge(string file1, string file2, string file3, int size, int& lenB, int& lenC) {
 	ofstream A(file1);
 	ifstream B(file2);
 	ifstream C(file3);
@@ -42,54 +86,99 @@ void merge(string file1, string file2, string file3, int size) {
 	string s2 = "";
 	int counter1 = 0;
 	int counter2 = 0;
-	getline(B, s1);
-	getline(C, s2);
-	while (!B.eof() || !C.eof()) {
+	if (lenB > 0) { getline(B, s1); }
+	if (lenC > 0) { getline(C, s2); }
+	while (lenB + lenC > 0) {
 		if (counter1 == size && counter2 < size) {
 			A << s2 << endl;
 			counter2++;
-			if (!C.eof() && counter2<size) {getline(C, s2);
-			}
-			else { counter2 = size; }
+			lenC--;
+			getline(C, s2);
 		}
 		else if (counter2 == size && counter1 < size) {
 			A << s1 << endl;
 			counter1++;
-			if (!B.eof() && counter1 < size) {
-				getline(B, s1);
-			}
-			else { counter1 = size; }
-			
+			lenB--;
+			getline(B, s1);
 		}
-		else if (counter2 < size && counter1 < size) {
-
-			if (get2word(s1) < get2word(s2)) {
+		else if (counter2 == size && counter1 == size) {
+			counter1 = 0;
+			counter2 = 0;
+		}
+		else if (counter1 < size && counter2 < size) {
+			if (compare_strings(get2word(s1), get2word(s2))) {
 				A << s1 << endl;
 				counter1++;
-				if (counter1<size) { getline(B, s1); }
+				lenB--;
+				getline(B, s1);
 			}
 			else {
 				A << s2 << endl;
 				counter2++;
-				if (counter2 < size) { getline(C, s2); }
+				lenC--;
+				getline(C, s2);
 			}
-		}
-		else if (counter2 == size && counter1 == size) {
-			getline(C, s2);
-			getline(B, s1);
-			if (!B.eof()) { counter1 = 0;}
-			if (!C.eof()) { counter2 = 0;};
 		}
 	}
 }
 
 
+void natural_partition(string file1, string file2, string file3, int size, int& lenB, int& lenC) {
+	ifstream A(file1);
+	ofstream B(file2);
+	ofstream C(file3);
+	string* buff = new string[size+1];
+	int counter = 0;
+	string s = "";
+	while (getline(A, s)) {
+		if (counter % (2*size) < size) {
+			buff[counter % (size)] = s;
+			if (counter % (size) == size-1 || A.eof()) {
+				quick_sort(buff, 0, counter % (size));
+				for (int i = 0; i < counter % (size);i++) {
+					cout << buff[i] << endl;
+					B << buff[i] << endl;
+				}
+			}
+		}
+		else if (counter % (2 * size) < 2*size) {
+			buff[counter % (size)] = s;
+			if (counter % (size) == size - 1 || A.eof()) {
+				quick_sort(buff, 0, counter % (size));
+				for (int i = 0; i < counter % (size); i++) {
+					cout << buff[i] << endl;
+					C << buff[i] << endl;
+				}
+			}
+		}
+		counter++;
+	}
+	cout << counter << endl;
+}
+
+
 int main()
 {
-	for (int i = 1; i < 35; i *= 2) {
-		partition("C:\\Users\\user\\Desktop\\A.txt", "C:\\Users\\user\\Desktop\\B.txt", "C:\\Users\\user\\Desktop\\C.txt", i);
-		merge("C:\\Users\\user\\Desktop\\A.txt", "C:\\Users\\user\\Desktop\\B.txt", "C:\\Users\\user\\Desktop\\C.txt", i);
-	}
+	int lenB = 0;
+	int lenC = 0;
+	string A = "C:\\Users\\user\\Desktop\\A.txt";
+	string B = "C:\\Users\\user\\Desktop\\B.txt";
+	string C = "C:\\Users\\user\\Desktop\\C.txt";
+	string* arr = new string[6]{" Vergil "," 2 ", " Light ", " 1 "," Acheron "};
+	quick_sort(arr, 0, 5);
+	cout << arr[0] << endl;
+	print_array(arr, 6);
+	//natural_partition(A, B, C, 3, lenB, lenC);
+	//for (int i = 0; i <= log2(35) ; i++) {
+	//	partition(A, B, C, pow(2.,i),lenB,lenC);
+	//	merge(A, B, C, pow(2., i),lenB,lenC);
+	//}
+	//partition(A, B, C, 1);
+	//merge(A, B, C, 1);
+	//cout << counterB<<endl;
+	//cout << counterC<<endl;
+	//A.close();
+	//merge("C:\\Users\\user\\Desktop\\A.txt", "C:\\Users\\user\\Desktop\\B.txt", "C:\\Users\\user\\Desktop\\C.txt", i);
 
 }
 
